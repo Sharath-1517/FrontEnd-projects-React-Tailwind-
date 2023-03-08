941a12222b32fb080b5b345ffc6e547ef074884b
sharath <sharathprof1517@gmail.com>
2023-03-08 21:06:01 +0530

    Some updates that made the app work.

--- src/component/Component.jsx
+++ src/component/Component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Component = (props) => {
   return (
       <div className="card">
-        <a href={props.live}><img src={`images/${props.img_path}`} alt="" className='card__image'/></a>
+        <Link to={props.live}><img src={`images/${props.img_path}`} alt="" className='card__image'/></Link>
         <h2 className="card__name">qr code component</h2>
         <Link to={props.live} className='card__link'>live url</Link>
         <a href={props.solution} className='card__solution__link'>solution url</a>
