c5fd28b526b7b34500cb66e0aaa2b2c75436e448
sharath <sharathprof1517@gmail.com>
2023-03-08 21:13:51 +0530

    Some changes in code.

--- src/component/Component.jsx
+++ src/component/Component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Component = (props) => {
   return (
       <div className="card">
-        <Link to={props.live}><img src={`images/${props.img_path}`} alt="" className='card__image'/></Link>
+        <img src={`images/${props.img_path}`} alt="" className='card__image'/>
         <h2 className="card__name">qr code component</h2>
         <Link to={props.live} className='card__link'>live url</Link>
         <a href={props.solution} className='card__solution__link'>solution url</a>
