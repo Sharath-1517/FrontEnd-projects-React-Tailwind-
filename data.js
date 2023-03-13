50496b622ab2b67830663cc5a9cdded0a7daf49a
sharath <sharathprof1517@gmail.com>
2023-03-13 18:59:51 +0530

    Added more screen props and changed some data.

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export default [
     {
         id: 1,
         challenge_name: "QR-Code Component",
-        solution: "",
+        solution: "https://github.com/Sharath-1517/FrontEnd-projects-React-Tailwind-/blob/main/src/component/qr-code/QR.jsx",
         live: "/FrontEnd-projects-React-Tailwind-/qr-code-challenge",
         img_path: "qr/qr-code.jpg",
         img_alt: "qr-code",
